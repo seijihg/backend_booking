d9ea2f5144fad7727420ff599720b31ccb885105
Le H Ngo <hgle.ngo@gmail.com>
2023-06-03 23:06:24 +0100

    -Adds detai, update and delete for Salon

--- salon/views.py
+++ salon/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from salon.models import Salon
 from salon.serializers import SalonSerializer
@@ -6,5 +6,10 @@ from salon.serializers import SalonSerializer
 
 # Create your views here.
 class SalonListCreateAPIView(ListCreateAPIView):
+    queryset = Salon.objects.all()
+    serializer_class = SalonSerializer
+
+
+class SalonDetailUpdateDeleteView(RetrieveUpdateDestroyAPIView):
     queryset = Salon.objects.all()
     serializer_class = SalonSerializer
