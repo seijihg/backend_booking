eb985570e5330251bb25af45968c8fe30afd0ed1
Le H Ngo <hgle.ngo@gmail.com>
2025-12-14 15:17:58 +0000

    Update salon serializer

--- salon/serializers.py
+++ salon/serializers.py
@@ -29,3 +29,33 @@ class SalonSerializer(serializers.ModelSerializer):
             salon.addresses.add(addr)
 
         return salon
+
+    def update(self, instance, validated_data):
+        address_data = validated_data.pop("addresses", None)
+
+        # Update salon fields
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        # Update addresses if provided
+        if address_data is not None:
+            # Clear existing addresses and add new ones
+            instance.addresses.clear()
+            for address in address_data:
+                address_id = address.get("id")
+                if address_id:
+                    # Update existing address
+                    addr = Address.objects.filter(id=address_id).first()
+                    if addr:
+                        for attr, value in address.items():
+                            if attr != "id":
+                                setattr(addr, attr, value)
+                        addr.save()
+                        instance.addresses.add(addr)
+                else:
+                    # Create new address
+                    addr = Address.objects.create(salon=instance, **address)
+                    instance.addresses.add(addr)
+
+        return instance
